78897489da88c77975b0246e135c88fd73a3c2fe
Haruyuki Ichino <mail@icchi.me>
2019-07-21 02:37:30 +0900

    Update: profile and refactoring with prettier

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <v-app dark>
     <!-- <Header/> -->
-    <Main/>
+    <Main />
   </v-app>
 </template>
 
 <script>
 // import Header from './components/Header'
-import Main from './components/Main'
+import Main from "./components/Main";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // Header,
     Main
-  },
+  }
   // data () {
   //   return {
   //     //
   //   }
   // }
-}
+};
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import "vuetify/src/stylus/app.styl";
 
 Vue.use(Vuetify, {
   // iconfont: 'md',
   theme: {
-    primary: '#2196F3',
-    secondary: '#E91E63',
+    primary: "#2196F3",
+    secondary: "#E91E63"
   }
-})
+});
